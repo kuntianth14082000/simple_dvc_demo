f70a86f5926b0166a7a7908e09c36079b0a70f44
KUNTINATH NORAJE <kuntinathnoraje@gmail.com>
2021-10-19 08:23:59 +0530

    created webapp file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,20 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self, message="values are not in range"):
+        # self.input = input_
+        self.message = message
+        super().__init__(self.message)
+
 
 def test_generic():
     a = 2
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange()
+
+def test_something():
+    a = 2
     b = 2
-    assert a == b
+    assert True
+
